5b589f3a215a70230201c91f0fc3a3f0edd9452e
alehandrolacayoz-ux <alehandrolacayoz@gmail.com>
2025-11-27 15:48:56 -0600

    He creado el evento para poner texto en el html

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const recuperarButton = document.querySelector(".btn");
+const recuperarDiv = document.querySelector('.app');
 
 recuperarButton.addEventListener("click", function(){
-alert("Hola estas disparando el evento");
+insertarTextoEnDiv();
 });
+
+function insertarTextoEnDiv(){
+    recuperarDiv.textContent = "Mierda culo69";
+}
